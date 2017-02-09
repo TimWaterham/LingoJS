a2a06242a911d66804e1cdd734aa8d93854198e9
Tim Waterham <99021474@mydavinci.nl>
2017-02-09 12:24:18 +0100

    begin of splitting word and checking input fields.

--- assets/js/lingo.js
+++ assets/js/lingo.js
@@ -1,2 +1,7 @@
-var randomWord = words[Math.floor(Math.random() * words.length)];
- 
+var randomWord = words[Math.floor(Math.random() * words.length)]; //get Random Word
+
+var wordArray = randomWord.split("", 5); //Split Random Word for checking
+
+this.check = function(id) {
+	let fields = [ id + ".1", id + ".2", id + ".3", id + ".4", id + ".5"];
+}
