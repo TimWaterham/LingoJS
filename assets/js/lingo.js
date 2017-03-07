a621316b03c4e6e48fbf214f9b35b3bf71891db5
Tim Waterham <99021474@mydavinci.nl>
2017-03-07 10:15:46 +0100

    Extending javascript / giving the HTML Check a ID

--- assets/js/lingo.js
+++ assets/js/lingo.js
@@ -2,6 +2,20 @@ var randomWord = words[Math.floor(Math.random() * words.length)]; //get Random W
 
 var wordArray = randomWord.split("", 5); //Split Random Word for checking
 
-this.check = function(id) {
-	let fields = [ id + ".1", id + ".2", id + ".3", id + ".4", id + ".5"];
+function check(id) {
+	var fields = [id + ".1", id + ".2", id + ".3", id + ".4", id + ".5"];
+	var result = "";
+	for (i = 0; i < fields.length; i++) {
+		if (result == "") {
+			result = document.getElementById(fields[i]).value;
+		}
+		else {
+			result = result + document.getElementById(fields[i]).value;
+		}
+		if (result[i] == wordArray[i]) {
+		}
+	}
+
+	return result;
 }
+
